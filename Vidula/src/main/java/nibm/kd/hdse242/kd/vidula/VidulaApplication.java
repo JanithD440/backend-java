494c2380cab95bd0eb359f7d1c3df2569125a705
nisalasandaruwan <nisalasandaruwan969@gmail.com>
2025-01-31 03:55:22 +0530

    BackEnd

---
+++ Vidula/src/main/java/nibm/kd/hdse242/kd/vidula/VidulaApplication.java
@@ -0,0 +1,13 @@
+package nibm.kd.hdse242.kd.vidula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidulaApplication.class, args);
+    }
+
+}
